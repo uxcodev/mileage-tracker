6b49cc7253bc8c2fad898eda67605e82dc974b45
Nick <nickatodd@gmail.com>
2025-03-06 08:34:42 -0800

    Initial Commit

---
+++ MileageTracker/MileageTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileageTrackerApp.swift
+//  MileageTracker
+//
+//  Created by Nick Todd on 2025-03-06.
+//
+
+import SwiftUI
+
+@main
+struct MileageTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MileageTrackerTests/MileageTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MileageTrackerTests.swift
+//  MileageTrackerTests
+//
+//  Created by Nick Todd on 2025-03-06.
+//
+
+import Testing
+@testable import MileageTracker
+
+struct MileageTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
